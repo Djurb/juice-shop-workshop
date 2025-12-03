d3d41bbec8f18e3b7fb05a65caad90982d865c4f
gerbensch <gerben.schepers@postcodeloterij.nl>
2025-12-03 14:48:30 +0100

    vibers

--- data/mongodb.ts
+++ data/mongodb.ts
@@ -3,11 +3,40 @@
  * SPDX-License-Identifier: MIT
  */
 
-// @ts-expect-error FIXME due to non-existing type definitions for MarsDB
-import MarsDB = require('marsdb')
+import { Low } from 'lowdb'
+import { Memory } from 'lowdb'
 
-const reviews = new MarsDB.Collection('posts')
-const orders = new MarsDB.Collection('orders')
+// Simple in-memory collections replacing MarsDB
+interface ReviewDoc { [key: string]: any }
+interface OrderDoc { [key: string]: any }
+
+const reviewsData: ReviewDoc[] = []
+const ordersData: OrderDoc[] = []
+
+const reviews = {
+  find: (query: any) => Promise.resolve(reviewsData.filter(r => Object.keys(query).every(k => r[k] === query[k]))),
+  update: (query: any, update: any, options?: any) => {
+    const matches = reviewsData.filter(r => Object.keys(query).every(k => r[k] === query[k]))
+    matches.forEach(r => Object.assign(r, update.$set || update))
+    return Promise.resolve({ modified: matches.length, original: matches })
+  },
+  insert: (doc: any) => { reviewsData.push(doc); return Promise.resolve(doc) },
+  remove: (query: any) => {
+    const initialLength = reviewsData.length
+    reviewsData.splice(0, reviewsData.length, ...reviewsData.filter(r => !Object.keys(query).every(k => r[k] === query[k])))
+    return Promise.resolve({ deleted: initialLength - reviewsData.length })
+  }
+}
+
+const orders = {
+  find: (query: any) => Promise.resolve(ordersData.filter(o => Object.keys(query).every(k => o[k] === query[k]))),
+  insert: (doc: any) => { ordersData.push(doc); return Promise.resolve(doc) },
+  remove: (query: any) => {
+    const initialLength = ordersData.length
+    ordersData.splice(0, ordersData.length, ...ordersData.filter(o => !Object.keys(query).every(k => o[k] === query[k])))
+    return Promise.resolve({ deleted: initialLength - ordersData.length })
+  }
+}
 
 const db = {
   reviews,
